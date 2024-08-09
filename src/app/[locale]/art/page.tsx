3219df6d7d069aff91b8592b9e00b7d364158d77
MariuszBednarz <mmbednarz90@gmail.com>
2024-08-09 14:11:16 +0200

    data querying init

--- src/app/[locale]/art/page.tsx
+++ src/app/[locale]/art/page.tsx
@@ -1,7 +1,38 @@
-export default function Art() {
+import { notFound } from "next/navigation";
+import { getClient } from "@/lib/apollo-client";
+import { gql } from "@apollo/client";
+
+const GET_ART = gql`
+  query GetPaintings {
+    arts(first: 100) {
+      id
+      createdAt
+      artYear
+      artType
+      artTitle
+      artImage(forceParentLocale: true) {
+        url
+        height
+        width
+      }
+    }
+  }
+`;
+
+export default async function Art({ params }: any) {
+  // if (true) {
+  //   notFound();
+  // }
+
+  const { data } = await getClient().query({ query: GET_ART });
+
+  console.log(data);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Art
+      {data.arts.map((el) => (
+        <div>{el.id}</div>
+      ))}
     </main>
   );
 }
